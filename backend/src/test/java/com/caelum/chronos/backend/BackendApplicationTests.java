cfebef6e22916ae65248ae893cff4cfe3d52497c
Matheuscrz <matheuslimasof.eng@gmail.com>
2026-06-04 12:12:08 -0300

    feat: Initialize backend project with Gradle and Spring Boot
    - Added Gradle wrapper files for project build management.
    - Created main application class `BackendApplication` with Spring Boot setup.
    - Configured application properties in `application.yml` for database and server settings.
    - Added a basic test class `BackendApplicationTests` to verify application context loading.
    - Defined project name in `settings.gradle`.

---
+++ backend/src/test/java/com/caelum/chronos/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caelum.chronos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
